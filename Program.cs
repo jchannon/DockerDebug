8ecacbf39e87dac8adadd3467eaa1de8f7719257
Jonathan Channon <jonathan.channon@gmail.com>
2017-06-06 09:07:26 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace jsonfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The file in csproj is marked as copy to output
+            //Therefore it will sit next to the app's dll in the Debug or Release folder
+            //For example, run dotnet run and it will work in Debug mode or run dotnet publish -c Release && cd bin/Release/netcoreapp1.1/publish && dotnet jsonfile.dll
+            var person = JsonConvert.DeserializeObject<Person>(File.ReadAllText("myfile.json"));
+            Console.WriteLine(person.Name + person.Age);
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
